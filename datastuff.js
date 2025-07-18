a76c5dbaeb0a4e46f91f59b2e45039d5fec2f53e
deltaomegathetac22707 <156774021+deltaomegathetac22707@users.noreply.github.com>
2025-07-18 14:17:35 -0500

    Add files via upload

---
+++ datastuff.js
@@ -0,0 +1,12 @@
+const data = {
+    "modpacks": [
+        {
+            "name": "Modpackname",
+            "ver": "1.0",
+            "mc_ver": "1.12.2",
+            "path": "modpack.jar"
+        }
+    ]
+};
+
+console.log(data.modpacks[0].name);
